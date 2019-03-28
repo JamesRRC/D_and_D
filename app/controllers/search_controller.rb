07e712ecdff8847a4f2b0d889542bb8c4395bc78
James Wilson <Jamesthewelder101@gmail.com>
2019-03-28 11:27:52 -0500

    blood pressure rising

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,11 +3,11 @@
 class SearchController < ApplicationController
   def results
     @query = params[:q]
-    @accessories = Accessory.where('accessory_name LIKE ?', "%#{@query}%")
+    @accessories_search = Accessory.where('accessory_name LIKE ?', "%#{@query}%")
 
     @hirelings = Hireling.where('name LIKE ?', "%#{@query}%")
 
     hire_classes = HireClass.where('class_name LIKE ?', "%#{@query}%")
-    @hire_classes = Hireling.where('hire_class_id = ?', "%#{hire_classes}%")
+    @hire_classes = Hireling.where('hire_class_id = ?', hire_classes)
   end
 end
