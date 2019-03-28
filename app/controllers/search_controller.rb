37f787d90122486c2e2b23487e166ec762f3f7aa
James Wilson <Jamesthewelder101@gmail.com>
2019-03-28 10:10:53 -0500

    struggling with category search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,5 +4,10 @@ class SearchController < ApplicationController
   def results
     @query = params[:q]
     @accessories = Accessory.where('accessory_name LIKE ?', "%#{@query}%")
+
+    @hirelings = Hireling.where('name LIKE ?', "%#{@query}%")
+
+    @hire_query = HireClass.where('class_name LIKE ?', "%#{@query}%")
+    @hire_classes = Hireling.where('hire_class_id == ?', "%#{@hire_query}%")
   end
 end
